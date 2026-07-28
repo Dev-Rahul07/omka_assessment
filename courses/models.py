75aa07b0fc5326cda0599b0ea6992e73c915e932
Rahul Pandit <pandit07.it@gmail.com>
2026-07-29 03:03:26 +0530

    Add Course and Module models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+# Course model
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='courses')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+# Module Model
+class Module(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='modules')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    order = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['order'] # basically sort the data in order  wise
+        unique_together = ('course', 'order')
+
+    def __str__(self):
+        return f"{self.course.title} - {self.title}"
