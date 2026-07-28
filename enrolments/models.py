a54c27ef507f70e20b65198be016a8edce427242
Rahul Pandit <pandit07.it@gmail.com>
2026-07-29 03:15:48 +0530

    Add Enrolment and Submission models

--- enrolments/models.py
+++ enrolments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from courses.models import Course
 
-# Create your models here.
+# Enrolment Model
+class Enrolment(models.Model):
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='enrolments')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='enrolments')
+    enrolled_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('student', 'course') # Basically mean that the student will not able enrol twice for the same course
+
+    def __str__(self):
+        return f"{self.student.username} -> {self.course.title}"
--- submissions/models.py
+++ submissions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from courses.models import Module
 
-# Create your models here.
+class Submission(models.Model):
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='submissions')
+    module = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='submissions')
+    content = models.FileField(upload_to='submissions/')
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.student.username} - {self.module.title}"
